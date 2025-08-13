4e09121e9525a19272a3d373b88717ccf02645fb
Nico-Yeremian <nyeremian09@gmail.com>
2025-08-12 17:36:50 -0700

    turned instructions into a list of steps; removed some print statements and open up the api window

--- main.py
+++ main.py
@@ -15,10 +15,15 @@ class API:
         self.model = "gpt-4.1-nano"
         self.cookbookLocation = "storage/cookbook.json"
         self.settingLocation = "storage/preferences.json"
+        self.processingRequest = False
 
     def create_recipe(self, user_preferences, prompt):
-        
-            
+        if self.processingRequest:
+            return
+
+
+        self.processingRequest = True
+
         gpt_job = "You are a chef who has expertise in making recipes, instructions, ingredients with nutrition facts, and a description of the food"
         
         if user_preferences != None:
@@ -27,9 +32,10 @@ class API:
         
         combinedPrompt = f"""{gpt_job} {user_preferences} and apply it when making the recipe. Knowing these restrictions, make a recipe for this food: {prompt}
         and format the recipe, Description of the food, then the ingredients with nutrition facts, and finally the instructions for the recipe.
+        Unless the user requests for the dish to have certain restrictions, such as being vegetarian or vegan, do NOT create a vegetarian or vegan recipe. Keep it regular unless the user requests.
         Follow this order and format when returning:
         - Name: A short, descriptive name of the food/dish in "str" or string format
-        - Ingredients: Make a list of all the ingredients used for the dish, all in "str" or string format  
+        - Ingredients: Make a bullet list of all the ingredients used for the dish, all in "str" or string format  
         - Nutrition: A bullet-list of all the nutrition facts of the dish, with a subheading labeled 'Nutrition Facts.' Make sure to make the list start underneath the label and move downward.
              - ServingsPerRecipe: The number of servings per recipe made with the given amout of ingredients
              - TotalCalories: The total amount of calories in one serving of the dish
@@ -40,7 +46,8 @@ class API:
         - Description: A more in-depth description of the dish in paragraph form, all in "str" or string format
           total carbohydrate, fiber, total sugars, protien, calcium, and iron, all in "str" or string format
         - Time: Time it takes to make the dish in "str" or string format
-        - Instructions: Write out the instructions to make the dish step-by step, with each step being part of a numbered list, again, all in "str" or string format"""
+        - Instructions: Write out the instructions to make the dish step-by step, with each step being part of a numbered list, again, all in "str" or string format.
+          Make sure to expand on each instruction and explain very in-depth what each step of making the dish involves"""
 
         
         response = self.client.beta.chat.completions.parse(
@@ -50,9 +57,9 @@ class API:
             ],
             response_format=Recipe
         )
-        print(type(response))
+        # print(type(response))
         response = response.choices[0].message.parsed
-        print(response)
+        # print(response)
         recipe_dict = {
             "name": response.name,
             "ingredients": response.ingredients,
@@ -69,6 +76,8 @@ class API:
             "time": response.time,
             "instructions": response.instructions
         }
+
+        self.processingRequest = False
 
         return recipe_dict
     
@@ -101,8 +110,12 @@ class API:
 
 if __name__  == "__main__":
     api = API()
-    pref = "not gluten"
-    prompt = "Pasta"
+    prompt = "pancakes"
+    pref = ""
+    
+    recipe = api.create_recipe(pref, prompt)
+    print(recipe)
+    
 
     html_path = os.path.abspath("static/index.html")
     window = webview.create_window("WeCookin",html_path,js_api=api)
